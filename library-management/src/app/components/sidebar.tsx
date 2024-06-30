2987369169efcfb53d8eae2450b76e3e81518a45
oengzhileang <zhileangCC990@gmail.com>
2024-06-30 18:58:56 +0700

    complete card and book detail page

--- library-management/src/app/components/sidebar.tsx
+++ library-management/src/app/components/sidebar.tsx
@@ -1,29 +1,33 @@
 // components/Sidebar.js
 'use client';
-import Button from "./button";// Assuming Button component file name is Button.js
+import Button from "./button"; // Assuming Button component file name is Button.js
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Sidebar = () => {
     const values = ["Dashboard", "Books", "Students", "Borrow", "Return"];
-    const [selected, setSelected] = useState<string | null>("Dashboard"); // Initialize selected state to null
-    
+    const [selected, setSelected] = useState<string>("Dashboard");
+    const router = useRouter();
+
     const handleClick = (item: string) => {
-        console.log(item); // This will log the value of the clicked button
-        setSelected(item); // Set selected item
+        console.log(item);
+        setSelected(item);
+        router.push(`/${item.toLowerCase()}`); // Navigate to the corresponding page
     };
-    
     return (
-        <div className="bg-[#1B2431]">
+        <div className="w-[256px]">
             <div className="h-screen w-[256px] bg-[#273142] text-white flex flex-col justify-between p-4">
                 <div>
-                    <h1 className="text-[40px] font-bold text-blue-500 mb-8 text-center">Ad<b className="text-white">min</b></h1>
+                    <h1 className="text-[40px] font-bold text-blue-500 mb-8 text-center">
+                        Ad<b className="text-white">min</b>
+                    </h1>
                     {values.map((item, index) => (
                         <Button
                             key={index}
                             onClick={() => handleClick(item)}
-                            selected={selected === item} // Pass selected prop to Button
+                            selected={selected === item}
                         >
-                            {item}
+                            {item}  
                         </Button>
                     ))}
                 </div>
